c2c844beb94ebf06a3c1bcbe5fb050f5e7eb25f0
abuzarshabab <58632229+abuzarshabab@users.noreply.github.com>
2021-07-24 14:25:00 +0530

    Add files via upload

---
+++ js/Index.js
@@ -0,0 +1,55 @@
+$(function(){
+    $("#includeNavbarAbove").load("navbarAbove.html"); 
+    });
+
+$(function(){
+    $("#includeNavbar").load("navbar.html"); 
+    });
+
+    $(function(){
+        $("#includeFooter").load("/footer.html"); 
+        });
+    
+    $(function(){
+        $("#includeFeedback").load("/includeFeedback.html"); 
+    });     
+
+    $(function(){
+        $("#includeFeedbackBlog").load("/includeFeedbackBlog.html"); 
+    });   
+
+    $(function(){
+        $("#includeCard").load("/includeCard.html")
+    })
+
+    $(function(){
+        $("#includeIt").load("/includeIt.html")
+    })
+
+    $(function(){
+        $("#includeIt").load("/includeIt.html")
+    })
+
+    $(function(){
+        $("#includeDigital").load("includeDigital.html")
+    })
+    $(function(){
+        $("#includeSideBar").load("/includeSideBar.html")
+    })
+    $(function(){
+        $("#includeContactForm").load("/includeContactForm.html")
+    })
+    $(function(){
+        $("#includeAdForm").load("/includeAdForm.html")
+    })
+
+//     let map;
+
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 8,
+//   });
+// }
+
+// AIzaSyBIwzALxUPNbatRBj3Xi1Uhp0fFzwWNBkE
